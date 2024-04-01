d18bf74129bb8c25fadf059764e8c69944b07f7b
Tushar Kapil <138205263+tushar1o@users.noreply.github.com>
2024-04-02 01:23:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+// var tablinks= document.getElementsByClassName("tab-links");
+// var tabcontents= document.getElementsByClassName("tab-contents");
+// function opentab(tabname){
+//     for(let tablink of tablinks){
+//         tablink.classList.remove("active-link")
+//     }
+//     for(let tabcontent of tabcontents){
+//         tabcontent.classList.remove("active-tab")
+//     }
+    
+//     event.currentTarget.classList.add("active-link");
+//     document.getElementById(tabname).classList.add("active-tab");
+//     // 41.29
+
+// }
+
+
+
+// // var tablinks = document.getElementsByClassName("tab-links");
+// // var tabcontents = document.getElementsByClassName("tab-contents");
+
+// // function opentab(event,tabname) {
+// //     tablinks.forEach(function(link){
+// //         link.classList. remove("active-link");
+// //     });
+
+// //     tabcontents.forEach(function(content){
+// //         content.classList.remove("active-tab");
+// //     });
+    
+
+    
+// //     event.currentTarget.classList.add("active-link");
+// //     document.getElementById(tabname).classList.add("active-tab");
+// // } 
+
+// // tablinks.forEach(function(link){
+// //     link.addEventListener("click", function(event){
+// //         var tabname= this.dateset.tabname;
+// //         opentab(event, tabname);
+// //     });
+// // });
+ 
+
